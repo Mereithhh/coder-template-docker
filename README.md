a28befed9d83c7febcec49c25bb917e6d114528f
WangLu <cs3cx4g@gmail.com>
2022-08-03 07:37:32 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # coder-template-docker
+
 my coder template docker image's dockerfile.
+
+just add some extension to code-server.
+
+languages:
+
+- [x] node
+- [x] go
+- [x] python
+- [x] rust
+- [ ] java
+- [ ] c++
+- [ ] c
+- [ ] c#
+- [ ] rocket
---
+++ build_push.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+NAME="base"
+cd base && docker build -t registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME} .
+docker push registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME}
+cd ..
+NAME="go"
+cd go && docker build -t registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME} .
+docker push registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME}
+cd ..
+NAME="java"
+cd java && docker build -t registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME} .
+docker push registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME}
+cd ..
+NAME="node"
+cd node && docker build -t registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME} .
+docker push registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME}
+cd ..
+NAME="python"
+cd python && docker build -t registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME} .
+docker push registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME}
+cd ..
+NAME="rust"
+cd rust && docker build -t registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME} .
+docker push registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME}
+cd ..
+NAME="vnc"
+cd vnc && docker build -t registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME} .
+docker push registry.cn-beijing.aliyuncs.com/mereith/coder-${NAME}
+cd ..
